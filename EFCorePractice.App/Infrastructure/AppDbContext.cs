b42a6f2715e527c6b6c4b4563c848ce5154d6ed8
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-05 17:13:20 -0500

    updated entity config in db context

--- EFCorePractice.App/Infrastructure/AppDbContext.cs
+++ EFCorePractice.App/Infrastructure/AppDbContext.cs
@@ -14,7 +14,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.Entity<Role>().ToTable(nameof(Role));
         modelBuilder.Entity<Role>().HasKey(r => r.Id).HasName("PK_Role");
         modelBuilder.Entity<Role>().Property(r => r.Name).IsRequired().HasMaxLength(30).IsUnicode(false);
-        //modelBuilder.Entity<Role>().Navigation(r => r.Accounts).AutoInclude(false);
         
         modelBuilder.Entity<Account>().ToTable(nameof(Account));
         modelBuilder.Entity<Account>().HasKey(a => a.Id).HasName("PK_Account");
@@ -24,5 +23,19 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             .HasForeignKey(e => e.RoleId)
             .OnDelete(DeleteBehavior.ClientSetNull)
             .HasConstraintName("FK_Account_Role");
+        modelBuilder.Entity<Account>()
+            .Ignore(a => a.Token);
+        modelBuilder.Entity<Account>()
+            .Property(a => a.Username).IsRequired().HasMaxLength(16).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .Property(a => a.PasswordHash).IsRequired().HasMaxLength(255).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .Property(a => a.PasswordSalt).IsRequired().HasMaxLength(255).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .Property(a => a.FirstName).IsRequired().HasMaxLength(50).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .Property(a => a.LastName).IsRequired().HasMaxLength(50).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .HasIndex(a => a.Username).IsUnique();
     }
 }
