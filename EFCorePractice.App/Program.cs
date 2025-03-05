2c819eb85c9ab05ae23ecdf65d0babec99a9fdb1
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-04 19:30:03 -0500

    registered db context in program.cs

--- EFCorePractice.App/Program.cs
+++ EFCorePractice.App/Program.cs
@@ -1,4 +1,9 @@
+
+using EFCorePractice.App.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("Server=.;Database=EFCorePractice;Trusted_Connection=true;Trust Server Certificate=true"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
