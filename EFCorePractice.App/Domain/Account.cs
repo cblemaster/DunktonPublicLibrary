1cf8ab177f2a13db6ddd7c69c12e47d74f0964ca
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-04 20:39:46 -0500

    added attributes to account entity

--- EFCorePractice.App/Domain/Account.cs
+++ EFCorePractice.App/Domain/Account.cs
@@ -6,4 +6,12 @@ public sealed class Account
     public Guid Id { get; set; }
     public Role Role { get; set; } = default!;
     public Guid RoleId { get; set; }
+
+    public string? Token { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string PasswordSalt { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
 }
