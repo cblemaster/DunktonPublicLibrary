3ad30b1fbf0607215391fce20b8a127f163e69a5
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-04 19:27:34 -0500

    added db context class

--- EFCorePractice.App/Domain/Account.cs
+++ EFCorePractice.App/Domain/Account.cs
@@ -5,4 +5,5 @@ public sealed class Account
 {
     public Guid Id { get; set; }
     public Role Role { get; set; } = default!;
+    public Guid RoleId { get; set; }
 }
---
+++ EFCorePractice.App/Infrastructure/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+using EFCorePractice.App.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePractice.App.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>().ToTable(nameof(Account));
+        modelBuilder.Entity<Account>().HasKey(a => a.Id).HasName("PK_Account");
+        modelBuilder.Entity<Role>().ToTable(nameof(Role));
+        modelBuilder.Entity<Role>().HasKey(r => r.Id).HasName("PK_Role");
+        modelBuilder.Entity<Role>().Property(r => r.Name).IsRequired().HasMaxLength(30).IsUnicode(false);
+        modelBuilder.Entity<Account>()
+            .HasOne(a => a.Role)
+            .WithMany(a => a.Accounts)
+            .HasForeignKey(e => e.RoleId)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK_Account_Role");
+    }
+}
