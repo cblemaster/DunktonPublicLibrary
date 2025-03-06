bb10fe6a9e9e34835b3edc9088f1d56242a5c9f8
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-05 22:04:57 -0500

    added private ctor and static create methods to identifier

--- EFCorePractice.App/Domain/Entities/Identifer.cs
+++ EFCorePractice.App/Domain/Entities/Identifer.cs
@@ -4,4 +4,9 @@ namespace EFCorePractice.App.Domain.Entities;
 public sealed class Identifer<T>
 {
     public Guid Value { get; init; }
+
+    private Identifer(Guid value) => Value = value;
+
+    public static Identifer<T> CreateNewId() => new(Guid.NewGuid());
+    public static Identifer<T> CreateFromData(Guid value) => new(value);
 }
