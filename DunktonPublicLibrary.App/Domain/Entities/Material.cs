f93f1ff164849a0c3aae52d04d7709c34fd141b0
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-13 19:31:22 -0400

    pulled common material attributes up to base class

--- DunktonPublicLibrary.App/Domain/Entities/Material.cs
+++ DunktonPublicLibrary.App/Domain/Entities/Material.cs
@@ -3,5 +3,8 @@ namespace DunktonPublicLibrary.App.Domain.Entities;
 
 public abstract class Material<T> : Entity<T>
 {
+    public string Title { get; init; }
+    public Genre Genre { get; init; }
+    public Identifer<Genre> GenreId { get; init; }
     public override Identifer<T> Id { get; init; }
 }
