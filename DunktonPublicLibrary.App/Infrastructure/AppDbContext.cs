20639eccbe2e0747661362596c9b0243c5907ccc
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-13 20:48:55 -0400

    added config of new entities to db context

--- DunktonPublicLibrary.App/Infrastructure/AppDbContext.cs
+++ DunktonPublicLibrary.App/Infrastructure/AppDbContext.cs
@@ -8,6 +8,10 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
 {
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Role> Roles { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Magazine> Magazines { get; set; }
+    public DbSet<VideoCassette> VideoCassettes { get; set; }
+    public DbSet<Genre> Genres { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -15,6 +19,59 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
         modelBuilder.Entity<Role>().HasKey(r => r.Id).HasName("PK_Role");
         modelBuilder.Entity<Role>().Property(r => r.Name).IsRequired().HasMaxLength(DataConstants.ROLE_NAME_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE);
         modelBuilder.Entity<Role>().Property(r => r.Id).HasConversion(i => i.Value, i => Identifer<Role>.CreateFromData(i));
+
+        modelBuilder.Entity<Genre>().ToTable(nameof(Genre));
+        modelBuilder.Entity<Genre>().HasKey(g => g.Id).HasName("PK_Genre");
+        modelBuilder.Entity<Genre>().Property(g => g.Name).IsRequired().HasMaxLength(DataConstants.GENRE_NAME_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE);
+        modelBuilder.Entity<Genre>().Property(g => g.Id).HasConversion(i => i.Value, i => Identifer<Genre>.CreateFromData(i));
+
+        modelBuilder.Entity<Book>().ToTable(nameof(Book));
+        modelBuilder.Entity<Book>().HasKey(b => b.Id).HasName("PK_Book");
+        modelBuilder.Entity<Book>().Property(b => b.Id).HasConversion(i => i.Value, i => Identifer<Material>.CreateFromData(i));
+        modelBuilder.Entity<Book>()
+            .HasOne(b => b.Genre)
+            .WithMany(g => g.Materials.Cast<Book>())
+            .HasForeignKey(e => e.GenreId)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK_Book_Genre");
+        modelBuilder.Entity<Book>()
+            .ComplexProperty(b => b.BookInfo).Property(b => b.Author).IsRequired().HasMaxLength(DataConstants.AUTHOR_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.AUTHOR_COLUMN);
+        modelBuilder.Entity<Book>()
+            .ComplexProperty(b => b.BookInfo).Property(m => m.Description).IsRequired().HasMaxLength(DataConstants.DESCRIPTION_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.DESCRIPTION_COLUMN);
+        modelBuilder.Entity<Book>()
+            .ComplexProperty(b => b.BookInfo).Property(m => m.PublicationYear).IsRequired().HasMaxLength(DataConstants.PUBLICATION_YEAR_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).IsFixedLength(true).HasColumnName(DataConstants.PUBLICATION_YEAR_COLUMN);
+
+        modelBuilder.Entity<Magazine>().ToTable(nameof(Magazine));
+        modelBuilder.Entity<Magazine>().HasKey(m => m.Id).HasName("PK_Magazine");
+        modelBuilder.Entity<Magazine>().Property(m => m.Id).HasConversion(i => i.Value, i => Identifer<Material>.CreateFromData(i));
+        modelBuilder.Entity<Magazine>()
+            .HasOne(b => b.Genre)
+            .WithMany(g => g.Materials.Cast<Magazine>())
+            .HasForeignKey(e => e.GenreId)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK_Magazine_Genre");
+        modelBuilder.Entity<Magazine>()
+            .ComplexProperty(m => m.MagazineInfo).Property(m => m.IssueNumber).IsRequired().HasMaxLength(DataConstants.ISSUE_NUMBER_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.ISSUE_NUMBER_COLUMN);
+        modelBuilder.Entity<Magazine>()
+            .ComplexProperty(m => m.MagazineInfo).Property(m => m.PublicationMonth).IsRequired().HasMaxLength(DataConstants.PUBLICATION_MONTH_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.PUBLICATION_MONTH_COLUMN);
+        modelBuilder.Entity<Magazine>()
+            .ComplexProperty(m => m.MagazineInfo).Property(m => m.PublicationYear).IsRequired().HasMaxLength(DataConstants.PUBLICATION_YEAR_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).IsFixedLength(true).HasColumnName(DataConstants.PUBLICATION_YEAR_COLUMN);
+
+        modelBuilder.Entity<VideoCassette>().ToTable(nameof(VideoCassette));
+        modelBuilder.Entity<VideoCassette>().HasKey(v => v.Id).HasName("PK_VideoCassette");
+        modelBuilder.Entity<VideoCassette>().Property(v => v.Id).HasConversion(i => i.Value, i => Identifer<Material>.CreateFromData(i));
+        modelBuilder.Entity<VideoCassette>()
+            .HasOne(b => b.Genre)
+            .WithMany(g => g.Materials.Cast<VideoCassette>())
+            .HasForeignKey(e => e.GenreId)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK_VideoCassette_Genre");
+        modelBuilder.Entity<VideoCassette>()
+            .ComplexProperty(v => v.VideoCassetteInfo).Property(v => v.Director).IsRequired().HasMaxLength(DataConstants.DIRECTOR_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.DIRECTOR_COLUMN);
+        modelBuilder.Entity<VideoCassette>()
+            .ComplexProperty(v => v.VideoCassetteInfo).Property(v => v.Rating).IsRequired().HasMaxLength(DataConstants.RATING_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).HasColumnName(DataConstants.RATING_COLUMN);
+        modelBuilder.Entity<VideoCassette>()
+            .ComplexProperty(v => v.VideoCassetteInfo).Property(v => v.ReleaseYear).IsRequired().HasMaxLength(DataConstants.RELEASE_YEAR_MAX_LENGTH).IsUnicode(DataConstants.IS_UNICODE).IsFixedLength().HasColumnName(DataConstants.RELEASE_YEAR_COLUMN);
 
         modelBuilder.Entity<Account>().ToTable(nameof(Account));
         modelBuilder.Entity<Account>().HasKey(a => a.Id).HasName("PK_Account");
