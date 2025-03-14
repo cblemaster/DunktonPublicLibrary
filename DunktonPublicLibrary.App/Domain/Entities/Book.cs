f026bfbec53be8651b62cfabcf31ba13994e6bf7
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-14 19:24:58 -0400

    code cleanup

--- DunktonPublicLibrary.App/Domain/Entities/Book.cs
+++ DunktonPublicLibrary.App/Domain/Entities/Book.cs
@@ -8,5 +8,5 @@ public sealed class Book : Material<Book>
     public BookInfo BookInfo { get; init; }
     public override Identifer<Material<Book>> Id { get; init; }
     public override Identifer<Book> ItemId { get; init; }
-    public override string Title { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public override string Title { get; init; }
 }
--- DunktonPublicLibrary.App/Domain/Entities/Magazine.cs
+++ DunktonPublicLibrary.App/Domain/Entities/Magazine.cs
@@ -6,7 +6,7 @@ namespace DunktonPublicLibrary.App.Domain.Entities;
 public sealed class Magazine : Material<Magazine>
 {
     public MagazineInfo MagazineInfo { get; init; }
-    public override string Title { get; init; }
     public override Identifer<Material<Magazine>> Id { get; init; }
     public override Identifer<Magazine> ItemId { get; init; }
+    public override string Title { get; init; }
 }
