f98c7cdaea5a7a3c7a49266a260149daa7691c9d
Brian LeMaster <brian.lemaster@outlook.com>
2025-03-13 20:48:28 -0400

    added data constants for new entities

--- DunktonPublicLibrary.App/DataConstants.cs
+++ DunktonPublicLibrary.App/DataConstants.cs
@@ -10,6 +10,15 @@ public static class DataConstants
     public const string LAST_NAME_COLUMN = "LastName";
     public const string CREATE_DATE_COLUMN = "CreateDate";
     public const string UPDATE_DATE_COLUMN = "UpdateDate";
+    public const string TITLE_COLUMN = "Title";
+    public const string AUTHOR_COLUMN = "Author";
+    public const string DESCRIPTION_COLUMN = "Description";
+    public const string PUBLICATION_YEAR_COLUMN = "PublicationYear";
+    public const string DIRECTOR_COLUMN = "Director";
+    public const string RATING_COLUMN = "Rating";
+    public const string RELEASE_YEAR_COLUMN = "ReleaseYear";
+    public const string ISSUE_NUMBER_COLUMN = "IssueNumber";
+    public const string PUBLICATION_MONTH_COLUMN = "PublicationMonth";
 
     public const int ROLE_NAME_MAX_LENGTH = 30;
     public const int USERNAME_MAX_LENGTH = 16;
@@ -19,6 +28,16 @@ public static class DataConstants
     public const int LAST_NAME_MAX_LENGTH = 50;
     public const int PLAINTEXT_PASSWORD_MAX_LENGTH = 16;
     public const int PLAINTEXT_PASSWORD_MIN_LENGTH = 4;
+    public const int GENRE_NAME_MAX_LENGTH = 30;
+    public const int TITLE_MAX_LENGTH = 100;
+    public const int AUTHOR_MAX_LENGTH = 100;
+    public const int DESCRIPTION_MAX_LENGTH = 255;
+    public const int PUBLICATION_YEAR_MAX_LENGTH = 4;
+    public const int DIRECTOR_MAX_LENGTH = 100;
+    public const int RATING_MAX_LENGTH = 8;
+    public const int RELEASE_YEAR_MAX_LENGTH = 4;
+    public const int ISSUE_NUMBER_MAX_LENGTH = 8;
+    public const int PUBLICATION_MONTH_MAX_LENGTH = 12;
 
     public const bool IS_UNICODE = false;
 }
